9b71bff3800cac5ed6ffad328eaedaf41cea4d66
sree <sreeraj_ec@hotmail.com>
2025-07-25 19:15:27 +0800

    20250725: Gateway mddleware working

--- services/gateway/app.js
+++ services/gateway/app.js
@@ -1,9 +1,30 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
+// Middleware
 app.use(express.json());
 
+// Users Service Routes
+app.use(
+  "/users",
+  createProxyMiddleware({
+    target: "http://user-service:4001",
+    changeOrigin: true,
+  })
+);
+
+// Notes Service Routes
+app.use(
+  "/notes",
+  createProxyMiddleware({
+    target: "http://notes-service:4002",
+    changeOrigin: true,
+  })
+);
+
+// Routes
 app.get("/", (req, res) => {
   res.json({
     message: "Entry Point",
